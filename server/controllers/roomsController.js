b5a4e9869ba80c2bc9939ddca98f8b2922df8789
marcio lira <machollanda@gmail.com>
2021-05-29 21:59:59 -0300

    create entities

--- server/controllers/roomsController.js
+++ server/controllers/roomsController.js
@@ -1,10 +1,48 @@
+import { constants } from "../src/util/constants.js"
+
 export default class RoomsController {
-    constructor() { }
+    #users = new Map()
+    constructor() { 
+        this.rooms = new Map()
+    }
 
     onNewConnection(socket) {
         const { id } = socket
         console.log('connection stablished with', id)
+        this.#updateGlobalUserData(id)
     }
+    joinRoom(socket, {user, room}) {
+
+        const userId = user.id = socket.id
+        const roomId = room.id
+
+        // console.log('connection stabilish with', id)
+        
+        const updateUserData = this.#updateGlobalUserData(
+            userId, 
+            user, 
+            roomId
+            )
+
+        socket.emit(constants.event.USER_CONNECTED, data)
+    }
+
+    #updateGlobalUserData(userId, userData = {}, roomId = '') {
+        
+        const user = users.get(userId) ?? {}
+        const existingRoom = this.rooms.has(roomId)
+        const updateUserData = new Atendee ({
+            ...user,
+            ...userData,
+            roomId,
+            // Se for o único na sala 
+            isSpeaker: !existingRoom,
+        })
+        this.#users.set(userId, updateUserData)
+
+        return this.#users.set(userId, updateUserData)
+    }
+
     getEvents() {
         const functions = Reflect.ownKeys(RoomsController.prototype)
         .filter(fn => fn !== 'constructor')
